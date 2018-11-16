e7a592d78fd4ad881fe46ea84700ace406218bd9
akbarabustang <akbar_abustang@ymail.com>
2018-11-16 18:20:06 +0700

    blading

---
+++ cms/resources/views/templates/backend/layouts/footer.blade.php
@@ -0,0 +1,42 @@
+    <!-- Jquery Core Js -->
+    <script src="{{ URL::asset('backend-template') }}/plugins/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="{{ URL::asset('backend-template') }}/plugins/bootstrap/js/bootstrap.js"></script>
+
+    <!-- Select Plugin Js -->
+    <script src="{{ URL::asset('backend-template') }}/plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+    <!-- Slimscroll Plugin Js -->
+    <script src="{{ URL::asset('backend-template') }}/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="{{ URL::asset('backend-template') }}/plugins/node-waves/waves.js"></script>
+
+    <!-- Jquery CountTo Plugin Js -->
+    <script src="{{ URL::asset('backend-template') }}/plugins/jquery-countto/jquery.countTo.js"></script>
+
+    <!-- Morris Plugin Js -->
+    <script src="{{ URL::asset('backend-template') }}/plugins/raphael/raphael.min.js"></script>
+    <script src="{{ URL::asset('backend-template') }}/plugins/morrisjs/morris.js"></script>
+
+    <!-- ChartJs -->
+    <script src="{{ URL::asset('backend-template') }}/plugins/chartjs/Chart.bundle.js"></script>
+
+    <!-- Flot Charts Plugin Js -->
+    <script src="{{ URL::asset('backend-template') }}/plugins/flot-charts/jquery.flot.js"></script>
+    <script src="{{ URL::asset('backend-template') }}/plugins/flot-charts/jquery.flot.resize.js"></script>
+    <script src="{{ URL::asset('backend-template') }}/plugins/flot-charts/jquery.flot.pie.js"></script>
+    <script src="{{ URL::asset('backend-template') }}/plugins/flot-charts/jquery.flot.categories.js"></script>
+    <script src="{{ URL::asset('backend-template') }}/plugins/flot-charts/jquery.flot.time.js"></script>
+
+    <!-- Sparkline Chart Plugin Js -->
+    <script src="{{ URL::asset('backend-template') }}/plugins/jquery-sparkline/jquery.sparkline.js"></script>
+
+    <!-- Custom Js -->
+    <script src="{{ URL::asset('backend-template') }}/js/admin.js"></script>
+    <script src="{{ URL::asset('backend-template') }}/js/pages/index.js"></script>
+
+    <!-- Demo Js -->
+    <script src="{{ URL::asset('backend-template') }}/js/demo.js"></script>
+    @yield('script')
